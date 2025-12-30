b263e085c753b10539a83c4eba9fc9b0dd4d5d4e
Vaibhav Prasad <vaibhavp220604@gmail.com>
2025-12-30 16:26:33 +0530

    fix: Updated CORS

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,16 @@ const PORT = process.env.PORT || 5000;
 const upload = multer({ dest: "./tmp/uploads" });
 
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://vaibhav8844.github.io/CCPD_TV/admin/',
+    'https://vaibhav8844.github.io/CCPD_TV/admin',
+    'https://vaibhav8844.github.io/CCPD_TV/client/',
+    'https://vaibhav8844.github.io/CCPD_TV/client',
+    'https://vaibhav8844.github.io/CCPD_TV',
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5000'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
